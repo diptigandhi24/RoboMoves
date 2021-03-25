95e21c0e7eb005fa0ed05e30b42d1975a6a33842
diptigandhi24 <gandhidipti.s@gmail.com>
2021-03-25 12:27:07 +0530

    Robo cannot be move beyond 0,0

--- src/customHooks/useRoboMove.test.ts
+++ src/customHooks/useRoboMove.test.ts
@@ -47,4 +47,20 @@ describe("checking all direction Robo move along with boundary points", () => {
     });
     expect(result.current.roboPosition).toEqual({ x: 1, y: 0 });
   });
+  test("Move Robo up from the initial position of 3,2", () => {
+    const { result } = renderHook(() => useRoboMove(5, { x: 3, y: 2 }));
+    act(() => {
+      /* fire events that update state */
+      result.current.updatePosition(AllowedPositions.UP);
+    });
+    expect(result.current.roboPosition).toEqual({ x: 2, y: 2 });
+  });
+  test("Robo stay at the same place when tried to move up from the initial position of 0,0", () => {
+    const { result } = renderHook(() => useRoboMove(5, { x: 1, y: 0 }));
+    act(() => {
+      /* fire events that update state */
+      result.current.updatePosition(AllowedPositions.UP);
+    });
+    expect(result.current.roboPosition).toEqual({ x: 0, y: 0 });
+  });
 });
